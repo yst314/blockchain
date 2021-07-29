c594daa1fc18a9eb0f00a007c5e1e9277047322f
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2021-07-29 12:10:57 +0900

    first

---
+++ blockchain.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from hashlib import sha256
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+        'timestamp': str(datetime.now()),
+        'proof': proof,
+        'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
